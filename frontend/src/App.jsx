b52889ae9db599eafcb2c8a1e8dba8da866895e3
jbrdge <jbrdge@users.noreply.github.com>
2026-04-01 10:55:13 -0700

    added feature for focusing on selected intersection (#14)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,8 @@ function App() {
   const [loading, setLoading] = useState(true);
   // state of hamburger menu
   const [isListOpen, setIsListOpen] = useState(false);
+  // selected intersection
+  const [selectedIntersection, setSelectedIntersection] = useState(null);
 
   
   useEffect(() => {
@@ -52,6 +54,15 @@ return (
             <div 
               key={item.intersection}
               className='intersection-list'
+              style={{ 
+                //conditionally style list object depending if the intersection is selected
+                backgroundColor: selectedIntersection === item.intersection ? '#9e9e9e' : 'white',
+                color: selectedIntersection === item.intersection ? 'white' : 'black' 
+              }}
+              onClick={() => setSelectedIntersection(
+                // clicking the intersection again nulls the selection
+                selectedIntersection === item.intersection ? null : item.intersection
+              )}
             >
               <div
                 className='list-indicator' 
@@ -81,7 +92,9 @@ return (
               pathOptions={{
                 color: getCongestionColor(intersection.congestion_score),
                 fillColor: getCongestionColor(intersection.congestion_score),
-                fillOpacity: 0.5,
+                // also condintionally set opacity for circle if intersection is selected
+                opacity: selectedIntersection === null || selectedIntersection === intersection.intersection ? 1 : 0.25,
+                fillOpacity: selectedIntersection === null || selectedIntersection === intersection.intersection ? 0.5 : 0.25
               }}
             >
               <Popup>
